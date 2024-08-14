6ff16aa1cd82fd38338ba74d715ee3cc44ecd1fc
Christopher <34822892+christophergouveia@users.noreply.github.com>
2024-08-14 11:45:37 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://chris:chris123@cluster0.xmab3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
